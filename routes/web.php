7ec01789fc2c1ba289f76f6be84133b143eee5ac
Stanely Macharia <kimanistanley45@gmail.com>
2026-02-15 11:57:07 +0300

    Redirect root URL to admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\TenantSigningController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin');
 });
 
 // Public lease verification routes (no auth required)
